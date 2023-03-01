26cd65146937ce2d6d2a2e81df1ce44972baaf53
ethan sherrick <112564954+Ethansherrick@users.noreply.github.com>
2023-02-28 19:46:51 -0600

    Added Footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
   
   return (
     <footer>
- 
+ Our story for this project is to make it easier for dog owners to network with others. This is to make play dates or walks so you and your dog can make friends and create a deeper bond.
     </footer>
   );
 };
