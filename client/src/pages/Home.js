d6fa4acbe177abdb9f7616bba62006b29d51ad0b
Eliseo <eliseo@Nidias-MacBook-Air.local>
2023-02-28 19:11:17 -0600

    added pitcha component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,27 @@ const Home = () => {
 
   return (
     <main>
-      hello
+      <div>
+        <p>
+        Does your dog ever need a friend to play with ?
+        tired of keeping your dog company
+
+
+        <div className='dog-pictures' >
+          <p className='picture-div'>
+            DOG Picture here
+        
+          </p>
+          <p className='picture-div'>
+            DOG Picture here
+           
+          </p>
+
+        </div>
+        </p>
+
+      </div>
+      
     </main>
   );
 };
