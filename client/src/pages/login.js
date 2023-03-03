abb0b12057fe6b79b26d2d28996dfc0798a1c518
Eliseo <eliseo@Nidias-MacBook-Air.local>
2023-03-02 20:56:33 -0600

    added login route

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../models/mutations';
+import { LOGIN_USER } from '../utils/mutations';
 
-import Auth from '../models/auth';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
