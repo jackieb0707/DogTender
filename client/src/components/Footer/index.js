2b584f7c33782c93ab76dc3fdffd849ded6bdfaa
ethan sherrick <112564954+Ethansherrick@users.noreply.github.com>
2023-03-02 21:16:00 -0600

    footer and link

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,9 +5,15 @@ const Footer = () => {
   
   return (
     <footer>
- Our story for this project is to make it easier for dog owners to network with others. This is to make play dates or walks so you and your dog can make friends and create a deeper bond.
- 
- 
+      <div>
+        <p>
+          Our story for this project is to make it easier for dog owners to network with others.
+          You can also help support animal shelters by voluntering!
+          <div>
+          <a href= "https://kcpetproject.org/" target="blank">Click Here</a> for more information on a local shelter!
+          </div>
+        </p>
+      </div>
     </footer>
   );
 };
