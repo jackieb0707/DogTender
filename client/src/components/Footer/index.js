ec65e67fdbb49bcd03694b55b8056f36fd653675
jackieb0707 <jackiebell446@yahoo.com>
2023-03-09 21:11:43 -0600

    readme

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,7 +8,8 @@ const Footer = () => {
       <div>
         <p>
           Our story for this project is to make it easier for dog owners to network with others. We care about dogs and wanted to help any owners
-          in need of assistance, we offer the best services when it comes to dogs!!
+          in need of assistance, we offer the best services when it comes to dogs!! For every dog love out there we hope this webpage will bring 
+          you and your dog happiness and friendships. every animal deserves to have a friend!!
           You can also help support animal shelters by volunteering!
           <div>
           <a href= "https://kcpetproject.org/" target="blank">Click Here</a> for more information on a local shelter!
